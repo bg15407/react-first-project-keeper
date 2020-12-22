79b2b600b10028b851a7edbd3f06f7741ff79545
Gayan Kalinga Bulathsinhala <bg15407@gmail.com>
2020-12-22 14:49:40 +0530

    Initial commit

---
+++ src/component/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Note Heading</h1>
+      <p>This is a note</p>
+    </div>
+  );
+}
+
+export default Note;
